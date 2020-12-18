c9399469ca587f04e4f651e3d0dc4d08c9619a2c
maringernina <nina.maringer01@htl-salzburg.ac.at>
2020-12-18 13:23:44 +0100

    add CountDays script and declare as well as assign the dates

---
+++ Assets/MyGame/Scripts/CountDays.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountDays : MonoBehaviour
+{
+
+    System.DateTime christmasDay;
+    System.DateTime firstAdvent;
+    System.DateTime secondAdvent;
+    System.DateTime thirdAdvent;
+    System.DateTime fourthAdvent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        christmasDay = new System.DateTime(2020, 12, 24);
+        firstAdvent = new System.DateTime(2020, 11, 29);
+        secondAdvent = new System.DateTime(2020, 12, 6);
+        thirdAdvent = new System.DateTime(2020, 12, 13);
+        fourthAdvent = new System.DateTime(2020, 12, 20);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
